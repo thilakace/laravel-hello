7e9fc806881d6e793b57ca6022f7dbd70c4afbe9
thilakace <thilakace@users.noreply.github.com>
2020-03-09 15:17:24 +0530

    asdas

--- src/HelloWorld/SayHello.php
+++ src/HelloWorld/SayHello.php
@@ -4,7 +4,7 @@ class SayHello
 {
     public static function world()
     {
-        return 'Hello World, Composer and updated!';
+        return 'Hello World, Composer and updated! this is another version';
     }
 
 }
